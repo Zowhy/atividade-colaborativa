125f78e904d8eef9958b91666370e5197498452e
Zowhy <dtrovato892@gmail.com>
2024-10-26 20:02:40 -0300

    Corrigir bug de exibição da data com id correto

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 function exibirDataAtual() {
     const dataAtual = new Date().toLocaleDateString();
-    document.getElementById("data").textContent = `Data de hoje: ${dataAtual}`;
+    document.getElementById("data-exibicao").textContent = `Data de hoje: ${dataAtual}`;
 }
-
-
-exibirDataAtual();
